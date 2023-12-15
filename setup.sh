9b812ec29091a019ebc84030d67ecb879d39bc92
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2023-12-15 21:07:11 +0000

    Fri Dec 15 09:07:11 PM UTC 2023

--- setup.sh
+++ setup.sh
@@ -3,6 +3,13 @@
 apt-get install kexec-tools fsmark libtool m4 automake make git bison flex sysstat fio trace-cmd tmux nfs-kernel-server nfs-common
 
 echo '/mnt *(rw,sync,no_subtree_check,no_root_squash)' > /etc/exports
+echo """dev=/dev/nvme0n1
+domain=40
+JOURNAL_DEV=/dev/nvme0n3
+FAST_COMMIT=1
+NFS_SERVER=0
+NFS_CLIENT=0
+""" > ~/fast-commit-override.sh
 cd filebench
 libtoolize
 aclocal
