690db7ffa5e2436a8d9611d8640d36da43b370f6
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2023-12-17 21:18:22 +0000

    Sun Dec 17 09:18:22 PM UTC 2023

--- bm/mkxfs.sh
+++ bm/mkxfs.sh
@@ -8,7 +8,5 @@ if [ "$JOURNAL_DEV" != "" ]; then
 	JRNL_MKFS="-l logdev=$JOURNAL_DEV"
 	JRNL_MNT="-o logdev=$JOURNAL_DEV"
 fi
-mkfs.xfs -f -d agsize=419614818304,sunit=256,swidth=1792 $JRNL_MKFS-l sunit=256 $1
-
-#mount $1 $2 -o nodev,noatime,attr2,inode64,sunit=256,swidth=1792,noquota
+mkfs.xfs -f $JRNL_MKFS -ssize=4k $1
 mount $1 $2 -o nodev,noatime,attr2,inode64,sunit=256,swidth=1792,noquota,nobarrier $JRNL_MNT
