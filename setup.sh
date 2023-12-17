b9115b376c277e41700201fbe2939d382bf001b9
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2023-12-17 21:07:07 +0000

    Sun Dec 17 09:07:07 PM UTC 2023

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install kexec-tools fsmark libtool m4 automake make git bison flex sysstat fio trace-cmd tmux nfs-kernel-server nfs-common
+apt-get install xfsprogs kexec-tools fsmark libtool m4 automake make git bison flex sysstat fio trace-cmd tmux nfs-kernel-server nfs-common
 
 echo '/mnt *(rw,sync,no_subtree_check,no_root_squash)' > /etc/exports
 echo """dev=/dev/nvme0n1
