7fb269b1ad7b96abb64ef08214ebb7f4ca974d61
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2023-12-19 06:50:46 +0000

    Tue Dec 19 06:50:46 AM UTC 2023

--- bm/parse.sh
+++ bm/parse.sh
@@ -7,7 +7,7 @@ source ./config
 echo -n "$RUN_ID,"
 if [ "$NFS_CLIENT" == "1" ]; then
     exit
-fi
+fi 
 
 function get_mb_written() {
     devname=${1:5}
@@ -15,30 +15,34 @@ function get_mb_written() {
     dev_kb_before=$(cat iostat_before.dat | grep $devname | xargs | cut -d " " -f7)
     echo -n $(((dev_kb_after-dev_kb_before)/1024))
 }
-    
-get_mb_written ${dev}
-echo -n ","
-if [ "$JOURNAL_DEV" != "" ]; then
-    get_mb_written ${JOURNAL_DEV}
-fi
 
-sum=0
+function get_wiops_sum() {
+	devname=${1:5}
+	sum=0
+	while read wiops; do
+    		sum=$(python3 -c "print($sum+$wiops)")
+	done < /tmp/wiops
+	echo $sum
+}
+    
+echo -n "$(get_mb_written ${dev}),$(get_mb_written ${JOURNAL_DEV}),$(get_wiops_sum $dev),$(get_wiops_sum $JOURNAL_DEV),"
+
 cat iostat.dat | grep ${dev:5} | sed -r 's/[[:blank:]]+/,/g' | cut -d "," -f8 > /tmp/wiops
 while read wiops; do
-    sum=$(python3 -c "print($sum+$wiops)")
+    dev_sum=$(python3 -c "print($sum+$wiops)")
 done < /tmp/wiops
 
 if [ "$NFS_SERVER" == "1" ]; then
     cd client-results
-    echo -n ",NFS"
+    echo -n "NFS=1,"
     source ./config
 else
-    echo -n ",Local"
+    echo -n "NFS=0."
 fi
-echo -n ":FC=$FAST_COMMIT:XFS=$XFS,$NUM_THREADS,$BENCHMARK"
+echo -n ":FC=$FAST_COMMIT:XFS=$XFS,$NUM_THREADS,$BENCHMARK,"
 if [[ "$BENCHMARK" == filebench* ]]; then
     runtime=$(cat result.dat | grep "Run took" | xargs | cut -d " " -f4)
 else
     runtime=60
 fi
-echo ",$runtime,$sum,$runtime"
+echo "$runtime"
