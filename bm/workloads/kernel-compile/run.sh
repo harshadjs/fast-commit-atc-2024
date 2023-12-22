f766c62f877d505f73909fce0ac3cad8c46120a4
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2023-12-22 21:08:52 +0000

    Fri Dec 22 09:08:51 PM UTC 2023

--- bm/workloads/kernel-compile/run.sh
+++ bm/workloads/kernel-compile/run.sh
@@ -6,5 +6,6 @@ cp config ${MNT}/.config
 cd $MNT
 time wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.144.tar.xz
 time tar -xf linux-5.15.144.tar.xz
+cd linux-5.15.144
 make olddefconfig
-time make -j 20
+time make -j 64
