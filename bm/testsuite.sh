467bb5e703ddedeea646a8f64d53168288ac90f1
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2024-01-02 09:19:25 +0000

    Tue Jan  2 09:19:25 AM UTC 2024

---
+++ bm/testsuite.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SERVER=$1
+
+if [ "$SERVER" == "1" ]; then
+  ./run_benchmark.sh configs/localssd-perf/server/
+  mv ~/results/ ~/localssd-perf
+  ./run_benchmark.sh configs/scaling/server/
+  mv ~/results/ ~/scaling
+else
+  ./run_benchmark.sh configs/localssd-perf/client/
+fi
